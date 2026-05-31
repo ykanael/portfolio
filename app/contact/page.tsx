6c72175a00dc5dab88dc1156f3bff30a98df6a3b
ykanael <gzfgvn79xz@privaterelay.appleid.com>
2026-05-31 19:49:31 +0200

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -22,7 +22,16 @@ export default function Contact() {
         email: formData.email,
         message: formData.message,
       };
+      
+      const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+      const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+      const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
 
+      if (!serviceID || !templateID || !publicKey) {
+        throw new Error("Les variables d'environnement EmailJS sont manquantes.");
+      }
+
+await emailjs.send(serviceID, templateID, templateParams, publicKey);
       // Appel à l'API EmailJS
       await emailjs.send(
         process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
